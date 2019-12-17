b283fb108349615238db550d9373008d9f80bd51
n19013 <n19013@std.it-college.ac.jp>
2019-12-17 20:38:04 +0900

    commit

---
+++ react_app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+  render () {
+      return <div className='App'>
+          <h2>Hallo,world!</h2>
+      </div>
+          }
+
+}
+
+export default App
