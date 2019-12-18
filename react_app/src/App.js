54adf6172b1a52b05f04e29a1efd2d4c44f0a1e1
n19013 <n19013@std.it-college.ac.jp>
2019-12-18 17:38:58 +0900

    jsx5

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react'
-import './App.css'
+import React from 'react'
 
-class App extends Component {
-render () {
-    return <div className='App'>
-        <h2>塔を立てる時は、まず座って費用を計算しよう</h2>
-    </div>
+const Hello = props => {
+  const value = '<<< 豚に真珠、猫に小判 >>>'
+  return <h2>{value}</h2>
 }
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <div>
+        <Hello />
+      </div>
+    )
+  }
 }
 
 export default App
