0f0da0d185f1d699f37763d522f6a4d307322cdb
n19013 <n19013@std.it-college.ac.jp>
2019-12-18 17:38:03 +0900

    jsx4

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,12 +1,36 @@
-import React, { Component } from 'react'
-import './App.css'
+import React from 'react'
 
-class App extends Component {
-render () {
-    return <div className='App'>
-        <h2>塔を立てる時は、まず座って費用を計算しよう</h2>
+const Hello = props => {
+  const css1 = {
+    color: 'red',
+    'background-color': '#f0f0ff',
+    'font-size': '2em'
+  }
+  const css2 = {
+    color: 'blue',
+    backgroundColor: '#fff0f0',
+    fontSize: '2em'
+  }
+  return (
+    <div>
+      <p style={css1}>探すのに時あり</p>
+      <p style={css2}>捨てるのに時がある</p>
     </div>
+  )
 }
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <div>
+        <Hello />
+      </div>
+    )
+  }
 }
 
 export default App
