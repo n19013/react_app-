071edd3d66e42af212444109dc6b6b53ef15a485
n19013 <n19013@std.it-college.ac.jp>
2019-12-19 15:55:18 +0900

    ライフサイクル

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react'
+class App extends Component {
 
-class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
-  }
-  render () {
-      return <div className='App'>
-          <h2>Hello,world!</h2>
-      </div>}
+constructor (props) {
+super(props)
+console.log('constructor')
+}
+componentWillMount () {
+console.log('componentWillMount')
+}
+componentDidMount () {
+console.log('componentDidMount')
+}
 
+shouldComponentUpdate (nextProps, nextState) {
+console.log('shouldComponentUpdate')
+return true
+}
+componentDidUpdate () {
+console.log('componentDidUpdate')
+}
+render () {
+console.log('render')
+    const setStateHandler = (e) => {
+    console.log('* call setState()')
+        this.setState({r: Math.random()})
+    }
+return (
+    <div>
+        <button onClick={setStateHandler}>
+            setState</button>
+    </div>
+)
+}
 }
 
 export default App
